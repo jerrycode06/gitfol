241828f81a3662fe8c2220dcf17118a9507d10f6
Jerry <dvcoolbuddy14@gmail.com>
2019-10-18 12:03:55 +0530

    Version 1

---
+++ loop.py
@@ -0,0 +1,8 @@
+"""list1 = ["Jerry",'X',45,4,"$%#",1,"Harry","CF",11,56,"Derry",5]
+for item in list1:
+    if str(item).isnumeric() and item>6:
+        print(item)"""
+a = int(input("ENter the input\n"))
+while a<100:
+    a = int(input("Enter the number again bro!!\n"))
+print("Congrats Man , You really Entered a number greater than 100")
